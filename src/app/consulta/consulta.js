4e80ba309d5bd4fdafb5376908fe3f04a136878b
Johan Cortes <johandcortes@gmail.com>
2023-07-06 17:28:06 -0500

    ‮

--- src/app/consulta/consulta.js
+++
@@ -1,6 +0,0 @@
-export default function consultar(){
-    console.log("consultar");
-    /*document.getElementById("boton1").addEventListener("click", (e) => {
-        console.log(e);
-    });*/
-};
